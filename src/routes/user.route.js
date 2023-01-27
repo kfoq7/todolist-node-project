d5a63d4eeaaa011bfa4787d947c5f2a2cf699a32
Edgardo Santos <easantose@gmail.com>
2023-01-27 00:11:32 -0500

    Added a user valifdation functions

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,15 +7,15 @@ import {
   loginUser,
 } from '../controllers/user.controllers.js'
 import { validate } from '../middlewares/validateFields.js'
-import { validateTask, validateUserId } from '../validators/index.js'
+import { validateTask, validateUserId, validationUser, validationLogin } from '../validators/index.js'
 
 const router = express.Router()
 
 router.get('/list', listUser)
 
-router.post('/register', registerUser)
+router.post('/register', validate([...validationUser]), registerUser)
 
-router.post('/login', loginUser)
+router.post('/login', validate([...validationLogin]), loginUser)
 
 router.get('/:userId', validate([...validateUserId]), listTaskUser)
 
--- src/validators/user-validator.js
+++ src/validators/user-validator.js
@@ -6,3 +6,35 @@ export const validateUserId = [
     .withMessage('The param id must be a mongoId')
     .bail(),
 ]
+
+export const validationUser = [
+  check('name')
+    .notEmpty()
+    .withMessage('The name is required')
+    .bail(),
+  check('email')
+    .isEmail()
+    .withMessage('The email is invalid')
+    .bail(),
+  check('password')
+    .isLength({ min: 6 })
+    .withMessage("The password must be at least 6 caractéres")
+    .bail(), 
+  check('confirm-pass')
+  .equals('password')
+  .withMessage("Password are not correct")
+    .bail()       
+]
+
+export const validationLogin = [
+  check('email')
+    .notEmpty()
+    .withMessage("The emails is required")
+    .isEmail()
+    .withMessage('The email is invalid')
+    .bail(),
+  check('password')
+    .notEmpty()
+    .withMessage("The password is required")
+    .bail(), 
+]
